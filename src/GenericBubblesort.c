75be1d108ca9ffc0d5075934a3555d3ec5ffe82f
csl <c_shengli@hotmail.com>
2014-06-09 10:47:01 +0800

    Optimise genericSingleBubbleSort()

--- src/GenericBubblesort.c
+++ src/GenericBubblesort.c
@@ -11,48 +11,30 @@ void genericSingleBubbleSort(void *array,int size, int length,
 	int length_1,length_2,indicator;
 	void *ori_array = array; // to store the original pointer of the array
 	int temp = length - 1 ; // temp to store number of elements of array;
-	char type ; // to be used as an indicator for different type
-		
+	int x ;	
 	if (compare == (&integerCompare) )
-	{
-		while ( temp != 0 ) // to change to pointer to point to the last element of array;
-			{
-				array +=4 ; // int 4byte for each array location
-				temp -- ;
-				type = 'i'; // indicate as integer type
-			}
-	}
-	
+		x = 4; // int 4byte
+
 	else if (compare == (&doubleCompare) )
-	{
-		while ( temp != 0 ) // to change to pointer to point to the last element of array;
-			{
-				array +=8 ; //double 8byte
-				temp -- ;
-				type = 'd'; //indicate as double type
-			}
-	}
+		x = 8;// double 8byte
 	
 	else 
-	{
-		while ( temp != 0 ) // to change to pointer to point to the last element of array;
-			{
-				array +=1 ;//char 1byte
-				temp -- ;
-				type = 'c'; //indicate as character type
-			}
-	}
+		x = 1;// char 1byte
 	
+	
+	while ( temp != 0 ) // to change to pointer to point to the last element of array;
+			{
+				array +=x ; 
+				temp -- ;
+			}
+	
+
 	while (size !=1)
 	{
 
 		void *xarray = array; 
-		if (type == 'i')
-			xarray -=4;				// point to the previous element of an array..eg current: array[1] xarray: array[0]
-		else if (type == 'd')
-			xarray -=8;
-		else 
-			xarray -=1;
+			xarray -=x;				// point to the previous element of an array..eg current: array[1] xarray: array[0]
+
 			
 		length_1 = length -1 ;
 		length_2 = length - 2 ;
@@ -65,12 +47,9 @@ void genericSingleBubbleSort(void *array,int size, int length,
 		
 		size -- ;
 		length -- ;
-		if (type == 'i')
-			array -=4;				
-		else if (type == 'd')
-			array -=8;
-		else 
-			array -=1;
+
+		array -=x;				
+
 	}
 }	
 
